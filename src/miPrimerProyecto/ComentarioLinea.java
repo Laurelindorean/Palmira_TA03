53e1f811b06b147cfe78276f195132609a68f276
Laurelindorean <kguinera@hotmail.com>
2023-03-07 16:13:31 +0100

    Entrega Tarea 3 del C1 por Palmira Romia

---
+++ src/miPrimerProyecto/ComentarioLinea.java
@@ -0,0 +1,15 @@
+package miPrimerProyecto;
+/**
+ * 
+ * @author Palmira
+ *
+ */
+public class ComentarioLinea {
+
+	public static void main(String[] args) {
+		//System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("FIN DE PROGRAMA");
+	}
+
+}
---
+++ src/miPrimerProyecto/ComentarioVariasLineas.java
@@ -0,0 +1,17 @@
+package miPrimerProyecto;
+
+public class ComentarioVariasLineas {
+
+	public static void main(String[] args) {
+		/*
+		 * System.out.prinln("Hola"); 
+		 * System.out.println("Adios");
+		 * System.out.println("Cucu");
+		 */
+		for (int i = 0; i < 6; /* ahora viene el paso */ i++)
+			System.out.print(i + " ");
+		System.out.println("FIN DEL PROGRAMA");
+
+	}
+
+}
